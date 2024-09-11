1377b96c060722b0f87ba342dccbe499c65d0845
piotrzalecki <piotrzalecki@gmail.com>
2024-09-11 19:29:04 +0100

    fix: Fixing initial codebase

---
+++ src/components/AppBody.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Web page placeholder</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
