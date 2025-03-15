b38b117cd4f9a1e65145fd61b2ab44035d707fde
piotrzalecki <piotrzalecki@gmail.com>
2025-03-15 21:38:23 +0000

    dev: Enchancing user experience

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,3 @@
-
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
     <div class="container-fluid">
@@ -16,92 +15,127 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+          <li v-if="store.token !== ''" class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home</router-link
+            >
           </li>
           <li v-if="store.token !== ''" class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" id="navBarDropDown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Admin</a>
+            <a
+              href="#"
+              class="nav-link dropdown-toggle"
+              id="navBarDropDown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              >Admin</a
+            >
 
             <ul class="dropdown-menu" aria-labelledby="navBarDropDown">
               <li>
-                <router-link class="dropdown-item" to="/admin/dashboard/transactions">Transactions</router-link>
+                <router-link
+                  class="dropdown-item"
+                  to="/admin/dashboard/transactions"
+                  >Transactions</router-link
+                >
               </li>
               <li>
-                <router-link class="dropdown-item" to="/admin/users">Manage Users</router-link>
+                <router-link class="dropdown-item" to="/admin/users"
+                  >Manage Users</router-link
+                >
               </li>
               <li>
-                <router-link class="dropdown-item" to="/admin/users/0">Add User</router-link>
+                <router-link class="dropdown-item" to="/admin/users/0"
+                  >Add User</router-link
+                >
               </li>
               <li>
-                <router-link class="dropdown-item" to="/admin/dashboard/tags">Tags</router-link>
+                <router-link class="dropdown-item" to="/admin/dashboard/tags"
+                  >Tags</router-link
+                >
               </li>
               <li>
-                <router-link class="dropdown-item" to="/admin/dashboard/budgets">Budgets</router-link>
+                <router-link class="dropdown-item" to="/admin/dashboard/budgets"
+                  >Budgets</router-link
+                >
               </li>
               <li>
-                <router-link class="dropdown-item" to="/admin/dashboard/logs">Logs</router-link>
+                <router-link class="dropdown-item" to="/admin/dashboard/logs"
+                  >Logs</router-link
+                >
               </li>
               <li>
-                <router-link class="dropdown-item" to="/admin/dashboard/transactions-recurrences">Transaction Recurrences</router-link>
+                <router-link
+                  class="dropdown-item"
+                  to="/admin/dashboard/transactions-recurrences"
+                  >Transaction Recurrences</router-link
+                >
               </li>
             </ul>
-
           </li>
 
           <li class="nav-item">
-            <router-link v-if="store.token == ''" class="nav-link" to="/login">Login</router-link>
-            <a href="javascript:void(0);" v-else class="nav-link" @click="logout">Logout</a>
+            <router-link v-if="store.token == ''" class="nav-link" to="/login"
+              >Login</router-link
+            >
+            <a
+              href="javascript:void(0);"
+              v-else
+              class="nav-link"
+              @click="logout"
+              >Logout</a
+            >
           </li>
         </ul>
 
         <span class="navbar-text">
-          {{ store.user.first_name ?? '' }}
+          {{ store.user.first_name ?? "" }}
         </span>
-
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import { store } from './store.js';
-import router from './../router/index.js'
-import Security from './security.js'
+import { store } from "./store.js";
+import router from "./../router/index.js";
+import Security from "./security.js";
 
 export default {
-  data(){
+  data() {
     return {
-      store
-    }
+      store,
+    };
   },
   methods: {
-    logout(){
+    logout() {
       const payload = {
         token: store.token,
-      }
+      };
 
-      fetch(process.env.VUE_APP_API_URL + "/users/logout", Security.requestOptions(payload))
-      .then((response) => response.json())
-      .then((response) => {
-        if (response.error) {
-          console.log(response.message);
-        } else {
-          store.token = "";
-          store.user = {};
+      fetch(
+        process.env.VUE_APP_API_URL + "/users/logout",
+        Security.requestOptions(payload)
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.error) {
+            console.log(response.message);
+          } else {
+            store.token = "";
+            store.user = {};
 
-          document.cookie = '_site_data=; Path=/; '
-          + 'SameSite=Strict; Secure; '
-          + 'Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+            document.cookie =
+              "_site_data=; Path=/; " +
+              "SameSite=Strict; Secure; " +
+              "Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 
-          router.push("/login");
-        }
-      })
-
-    
-    }
-  }
+            router.push("/login");
+          }
+        });
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/AppLoginComposition.vue
+++ src/components/AppLoginComposition.vue
@@ -22,7 +22,7 @@
             required="true"
           >
           </text-input>
-      
+
           <hr />
           <input type="submit" class="btn btn-primary" value="Login" />
         </form-tag>
@@ -33,68 +33,69 @@
 <script>
 import TextInput from "./forms/TextInput.vue";
 import FormTag from "./forms/FormTag.vue";
-import { store } from './store.js';
-import router from '../router/index.js'
+import { store } from "./store.js";
+import router from "../router/index.js";
 
-import Security from './security.js'
-import {ref} from 'vue'
-
+import Security from "./security.js";
+import { ref } from "vue";
 
 export default {
   name: "AppLoginComposition",
-  emits: ['error'],
+  emits: ["error"],
   props: {},
   components: {
-    'form-tag': FormTag,
-    'text-input': TextInput,
+    "form-tag": FormTag,
+    "text-input": TextInput,
   },
 
-  setup(props, cxt){
-    let email = ref("")
-    let password = ref("")
-
+  setup(props, cxt) {
+    let email = ref("");
+    let password = ref("");
 
     function submitHandler() {
       const payload = {
         email: email.value,
         password: password.value,
-      }
+      };
 
-      fetch(process.env.VUE_APP_API_URL + "/users/login", Security.requestOptions(payload))
-      .then((response) => response.json())
-      .then((response) => {
-        if (response.error) {
-          cxt.emit('error', response.message)
-        } else {
-          store.token = response.data.token.token
-          store.user = {
-            id: response.data.user.id,
-            first_name: response.data.user.first_name,
-            last_name: response.data.user.last_name,
-            email: response.data.user.email,
+      fetch(
+        process.env.VUE_APP_API_URL + "/users/login",
+        Security.requestOptions(payload)
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.error) {
+            cxt.emit("error", response.message);
+          } else {
+            store.token = response.data.token.token;
+            store.user = {
+              id: response.data.user.id,
+              first_name: response.data.user.first_name,
+              last_name: response.data.user.last_name,
+              email: response.data.user.email,
+            };
+            let date = new Date();
+            let expDays = 1;
+            date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+            const expires = "expires=" + date.toUTCString();
+
+            document.cookie =
+              "_site_data=" +
+              JSON.stringify(response.data) +
+              "; " +
+              expires +
+              "; path=/; SameSite=strict; Secure;";
+
+            router.push("/");
           }
-
-          let date = new Date();
-          let expDays = 1;
-          date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
-          const expires = "expires=" + date.toUTCString();
-
-          document.cookie = "_site_data="
-          + JSON.stringify(response.data)
-          + "; "
-          + expires
-          + "; path=/; SameSite=strict; Secure;"
-
-          router.push("/");
-        }
-      })
-
-
+        });
     }
 
     return {
-      email, password, submitHandler
-    }
-
-}}
+      email,
+      password,
+      submitHandler,
+    };
+  },
+};
 </script>
